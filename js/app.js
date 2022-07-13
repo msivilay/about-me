8b1ced55e422960411c9e36ac5b927e6db03f2bd
Marina Sivilay <msivilay@gmail.com>
2022-07-13 01:35:53 -0700

    Added more assignment content

--- js/app.js
+++ js/app.js
@@ -1,21 +1,80 @@
 'use script';
+
 console.log('hello world');
-let livesInSeattle = prompt('Do I live in Seattle? y/n');
-//console.log(livesInSeattle);
 
-if 
+let userName = prompt('Greetings, what is your name?');
+console.log(userName);
 
-//(livesInSeattle === 'y' || livesinSeattle === 'Yes'){
-//  console.log(livesinSeattle.);
-//let lowerCaseLivesinSeattle = livesinSeattle.toLowerCase();
-//console.log(lowerCaseLivesinSeattle)
+alert('Welcome, ' + userName + '!' + ' Let\'s see how well you know me...');
 
-or livesInSeattle.toLowerCase();
+let fromChicago = prompt('Am I from Chicago (y/n)?').toLowerCase();
+// console.log(fromChicago);
 
+if (fromChicago === 'yes' || fromChicago === 'y') {
+//  console.log('Yes, I am from Chicago!');
+  alert('Yes, I am from Chicago!');
+} else if (fromChicago === 'no' || fromChicago === 'n') {
+//  console.log('Sorry, you are wrong. I am from Chicago.');
+  alert('Sorry, you are wrong. I am from Chicago.');
+} else {
+//  console.log('You need to answer with a \'yes\' or a \'no\'');
+  alert('You need to answer with a \'yes\' or a \'no\'');
+}
 
+let dogs = prompt('Do I hate dogs (y/n)?').toLowerCase();
+// console.log(dogs);
 
+if (dogs === 'yes' || dogs === 'y') {
+//  console.log('Sorry, but you are wrong. Dogs rule!');
+  alert('Sorry, but you are wrong. Dogs rule!');
+} else if (dogs === 'no' || dogs === 'n') {
+//  console.log('Yup! Dogs are the best!');
+  alert('Yup! Dogs are the best!');
+} else {
+//  console.log('You need to answer with a \'yes\' or a \'no\'');
+  alert('You need to answer with a \'yes\' or a \'no\'');
+}
 
+let teacher = prompt('Was I a teacher (y/n)?').toLowerCase();
+// console.log(teacher);
 
-  else{
-    console.log("Sorry, you're wrong.");
-  }
+if (teacher === 'yes' || teacher === 'y') {
+//  console.log('Yes, I was a high school teacher.');
+  alert('Yes, I was a high school teacher.');
+} else if (teacher === 'no' || teacher === 'n') {
+//  console.log('Sorry, incorrect. See me after class.');
+  alert('Sorry, incorrect. See me after class.');
+} else {
+//  console.log('You need to answer with a \'yes\' or a \'no\'');
+  alert('You need to answer with a \'yes\' or a \'no\'');
+}
+
+let dance = prompt('Do I know how to dance (y/n)?').toLowerCase();
+// console.log(dance);
+
+if (dance === 'yes' || dance === 'y') {
+//  console.log('I sure do, let\'s boogie\!');
+  alert('I sure do, let\'s boogie!');
+} else if (dance === 'no' || dance === 'n') {
+//  console.log('Wrong: Everyone can dance!');
+  alert('Wrong: Everyone can dance!');
+} else {
+//  console.log('You need to answer with a \'yes\' or a \'no\'');
+  alert('You need to answer with a \'yes\' or a \'no\'');
+}
+
+let SPU = prompt('Did I attend Seattle Pacific University (y/n)?').toLowerCase();
+// console.log(SPU);
+
+if (SPU === 'yes' || SPU === 'y') {
+//  console.log('Yes, I both worked and went to school at SPU!');
+  alert('Yes, I both worked and went to school at SPU!');
+} else if (SPU === 'no' || SPU === 'n') {
+//  console.log('Sorry, wrong anwer.');
+  alert('Sorry, wrong anwer.');
+} else {
+//  console.log('You need to answer with a \'yes\' or a \'no\'');
+  alert('You need to answer with a \'yes\' or a \'no\'');
+}
+
+alert('Thanks for playing, ' + userName +'.');
