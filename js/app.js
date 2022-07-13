10c91a2b662b8269b2ed15ad74e3342650b83db0
Marina Sivilay <msivilay@gmail.com>
2022-07-12 18:42:16 -0700

    Creating required tree structure and adding 2 linters and .gitignore

---
+++ js/app.js
@@ -0,0 +1,21 @@
+'use script';
+console.log('hello world');
+let livesInSeattle = prompt('Do I live in Seattle? y/n');
+//console.log(livesInSeattle);
+
+if 
+
+//(livesInSeattle === 'y' || livesinSeattle === 'Yes'){
+//  console.log(livesinSeattle.);
+//let lowerCaseLivesinSeattle = livesinSeattle.toLowerCase();
+//console.log(lowerCaseLivesinSeattle)
+
+or livesInSeattle.toLowerCase();
+
+
+
+
+
+  else{
+    console.log("Sorry, you're wrong.");
+  }
